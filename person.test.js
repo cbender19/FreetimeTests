6b9b87f9f60a47230a02e16c84389a2ceb6f8c1c
Connor Bender <83254356+cbender19@users.noreply.github.com>
2021-10-26 17:45:25 -0500

    Add files via upload

--- person.test.js
+++ person.test.js
@@ -1,10 +1,31 @@
-const firstname = require('./person');
-const lastname = require('./person');
+function firstname(firstname1,firstname2){
+  if(firstname1 === firstname2){
+    return firstname1;
+  }
+  else if(firstname1.localeCompare(firstname2)){
+    return firstname1;
+  }
+  else {
+    return null;
+  }
+}
+
+function lastname(lastname1, lastname2){
+   if(lastname1 === lastname2){
+     return lastname1;
+   }
+   else if(lastname1.localeCompare(lastname2)){
+     return lastname1;
+   }
+   else {
+     return null;
+   }
+}
 
 describe("person comparison", () => {
   test("it should compare two people by first and last name", () => {
-    expect(firstname("Connor", "Connor")).toBe("Connor");
+    expect(firstname("Connor", "connor")).toBe("Connor");
 
-    expect(lastname("Bender", "Bender")).toBe("Bender");
+    expect(lastname("Bender", "bender")).toBe("Bender");
   });
 });
